8921b0f1b2e0a10b17fd4172d59960f1d8330a83
Reece Arratoonian-Walker <reece.arratoonianwalker@gmail.com>
2025-10-13 16:02:53 +0100

    Add QR code support

--- epson_tmt20iii_test.go
+++ epson_tmt20iii_test.go
@@ -3,6 +3,7 @@ package escpos
 import (
 	"bytes"
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -17,6 +18,7 @@ func TestEpsonTMT20III_SimpleCommands(t *testing.T) {
 		{"init command returns correct value", profile.InitCommand, []byte{'\x1B', '@'}},
 		{"cut command returns correct value", profile.CutCommand, []byte{'\x1D', 'V', 'A', '0'}},
 		{"end command returns correct value", profile.EndCommand, []byte{'\xFA'}},
+		{"print qr code command returns correct value", profile.PrintQrCodeDataCommand, []byte{'\x1D', '(', 'k', 3, 0, 49, 81, 48}},
 	}
 
 	for _, testCase := range cases {
@@ -252,4 +254,200 @@ func TestEpsonTMT20III_CharSizeCommand(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestEpsonTMT20III_SelectQrCodeModelCommand(t *testing.T) {
+	var profile Profile = EpsonTMT20III{}
+
+	cases := []struct {
+		name  string
+		model string
+		want  []byte
+	}{
+		{"qr code model 1 returns correct value", "1", []byte{'\x1D', '(', 'k', 4, 0, 49, 65, 49, 0}},
+		{"qr code model 2 returns correct value", "2", []byte{'\x1D', '(', 'k', 4, 0, 49, 65, 50, 0}},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got, err := profile.SelectQrCodeModelCommand(&QrCodeConfig{model: testCase.model})
+
+			if err != nil {
+				t.Errorf("err was not nil")
+			}
+
+			gotAsBytes := []byte(got)
+
+			if !bytes.Equal(gotAsBytes, testCase.want) {
+				t.Errorf("SelectQrCodeModelCommand did not return expected bytes: wanted %v, got %v", testCase.want, gotAsBytes)
+			}
+		})
+	}
+
+	t.Run("unknown qr code model returns error", func(t *testing.T) {
+		got, err := profile.SelectQrCodeModelCommand(&QrCodeConfig{model: "micro"})
+
+		if got != "" || err == nil {
+			t.Errorf("returned command was not nil, expected empty string and error")
+		}
+
+		expectedError := fmt.Sprintf("invalid model in QrCodeConfig: %v\n", "micro")
+
+		if err.Error() != expectedError {
+			t.Errorf("SelectQrCodeModelCommand did not return expected error, got %s, wanted %s", err.Error(), expectedError)
+		}
+	})
+}
+
+func TestEpsonTMT20III_SetQrCodeSizeCommand(t *testing.T) {
+	var profile Profile = EpsonTMT20III{}
+
+	cases := []struct {
+		name string
+		size uint
+		want []byte
+	}{
+		{"qr code size 1 returns correct value", 1, []byte{'\x1D', '(', 'k', 3, 0, 49, 67, 1}},
+		{"qr code size 2 returns correct value", 2, []byte{'\x1D', '(', 'k', 3, 0, 49, 67, 2}},
+		{"qr code size 16 returns correct value", 16, []byte{'\x1D', '(', 'k', 3, 0, 49, 67, 16}},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got, err := profile.SetQrCodeSizeCommand(&QrCodeConfig{size: testCase.size})
+
+			if err != nil {
+				t.Errorf("err was not nil")
+			}
+
+			gotAsBytes := []byte(got)
+
+			if !bytes.Equal(gotAsBytes, testCase.want) {
+				t.Errorf("SetQrCodeSizeCommand did not return expected bytes: wanted %v, got %v", testCase.want, gotAsBytes)
+			}
+		})
+	}
+
+	negativeCases := []struct {
+		name string
+		size uint
+	}{
+		{"qr code size 0 returns error", 0},
+		{"qr code size 17 returns error", 17},
+	}
+
+	for _, testCase := range negativeCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got, err := profile.SetQrCodeSizeCommand(&QrCodeConfig{size: testCase.size})
+
+			if got != "" || err == nil {
+				t.Errorf("returned command was not nil, expected empty string and error")
+			}
+
+			expectedError := fmt.Sprintf("invalid size option in QrCodeConfig: %v\n", testCase.size)
+
+			if err.Error() != expectedError {
+				t.Errorf("SetQrCodeSizeCommand did not return expected error, got %s, wanted %s", err.Error(), expectedError)
+			}
+		})
+	}
+}
+
+func TestEpsonTMT20III_SelectQrCodeErrorCorrectionLevelCommand(t *testing.T) {
+	var profile Profile = EpsonTMT20III{}
+
+	cases := []struct {
+		name  string
+		level string
+		want  []byte
+	}{
+		{"qr code error correction level L returns correct value", "L", []byte{'\x1D', '(', 'k', 3, 0, 49, 69, 48}},
+		{"qr code error correction level M returns correct value", "M", []byte{'\x1D', '(', 'k', 3, 0, 49, 69, 49}},
+		{"qr code error correction level Q returns correct value", "Q", []byte{'\x1D', '(', 'k', 3, 0, 49, 69, 50}},
+		{"qr code error correction level H returns correct value", "H", []byte{'\x1D', '(', 'k', 3, 0, 49, 69, 51}},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got, err := profile.SelectQrCodeErrorCorrectionLevelCommand(&QrCodeConfig{errorCorrection: testCase.level})
+
+			if err != nil {
+				t.Errorf("err was not nil")
+			}
+
+			gotAsBytes := []byte(got)
+
+			if !bytes.Equal(gotAsBytes, testCase.want) {
+				t.Errorf("SelectQrCodeErrorCorrectionLevelCommand did not return expected bytes: wanted %v, got %v", testCase.want, gotAsBytes)
+			}
+		})
+	}
+
+	negativeCases := []struct {
+		name  string
+		level string
+	}{
+		{"qr code error correction level X returns error", "X"},
+		{"qr code error correction level unknown returns error", "unknown"},
+	}
+
+	for _, testCase := range negativeCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got, err := profile.SelectQrCodeErrorCorrectionLevelCommand(&QrCodeConfig{errorCorrection: testCase.level})
+
+			if got != "" || err == nil {
+				t.Errorf("returned command was not nil, expected empty string and error")
+			}
+
+			expectedError := fmt.Sprintf("invalid error correction level option in QrCodeConfig: %v\n", testCase.level)
+
+			if err.Error() != expectedError {
+				t.Errorf("SelectQrCodeErrorCorrectionLevelCommand did not return expected error, got %s, wanted %s", err.Error(), expectedError)
+			}
+		})
+	}
+}
+
+func TestEpsonTMT20III_StoreQrCodeDataCommand(t *testing.T) {
+	var profile Profile = EpsonTMT20III{}
+
+	cases := []struct {
+		name string
+		data string
+		want []byte
+	}{
+		{"qr code store data command returns correct value for 'hello'", "hello", []byte{'\x1D', '(', 'k', 8, 0, 49, 80, 48, 'h', 'e', 'l', 'l', 'o'}},
+		{"qr code store data command returns correct value for '123'", "123", []byte{'\x1D', '(', 'k', 6, 0, 49, 80, 48, '1', '2', '3'}},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got, err := profile.StoreQrCodeDataCommand(testCase.data)
+
+			if err != nil {
+				t.Errorf("err was not nil")
+			}
+
+			gotAsBytes := []byte(got)
+
+			if !bytes.Equal(gotAsBytes, testCase.want) {
+				t.Errorf("StoreQrCodeDataCommand did not return expected bytes: wanted %v, got %v", testCase.want, gotAsBytes)
+			}
+		})
+	}
+
+	t.Run("qr code store data command returns error for large data", func(t *testing.T) {
+		data := strings.Repeat("testingonetwotestinglongdatahi", 264)
+		got, err := profile.StoreQrCodeDataCommand(data)
+
+		if got != "" || err == nil {
+			t.Errorf("returned command was not nil, expected empty string and error")
+		}
+
+		expectedError := fmt.Sprintf("maximum data length exceeded: %v > 7086 (max)\n", len(data))
+
+		if err.Error() != expectedError {
+			t.Errorf("StoreQrCodeDataCommand did not return expected error, got %s, wanted %s", err.Error(), expectedError)
+		}
+	})
 }
---
+++ qrcode.go
@@ -0,0 +1,42 @@
+package escpos
+
+type QrCodeConfig struct {
+	model           string
+	size            uint
+	errorCorrection string
+	justification   string
+}
+
+func DefaultQrCodeConfig() QrCodeConfig {
+	return QrCodeConfig{
+		model:           "2",
+		size:            3,
+		errorCorrection: "L",
+		justification:   "center",
+	}
+}
+
+// Model sets the QR code model. The default is model 1.
+func (cfg QrCodeConfig) Model(model string) QrCodeConfig {
+	cfg.model = model
+	return cfg
+}
+
+// Size sets the QR code size. The default is 3.
+func (cfg QrCodeConfig) Size(size uint) QrCodeConfig {
+	cfg.size = size
+	return cfg
+}
+
+// Justify sets the QR code justification. The default is center.
+func (cfg QrCodeConfig) Justify(justification string) QrCodeConfig {
+	cfg.justification = justification
+	return cfg
+}
+
+// ErrorCorrection sets the error correction level. The default is
+// level L.
+func (cfg QrCodeConfig) ErrorCorrection(level string) QrCodeConfig {
+	cfg.errorCorrection = level
+	return cfg
+}
